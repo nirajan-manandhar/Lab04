a5dfc20cf2deac6be39f0b143b460929a498b24c
nirajan-manandhar <manandnirajan@gmail.com>
2018-10-11 14:56:08 -0700

    Initial commit

---
+++ animal.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nirajan on 2018-10-11.
+//
+
+#include "animal.hpp"
---
+++ animal.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Nirajan on 2018-10-11.
+//
+
+#ifndef LAB4_ANIMAL_HPP
+#define LAB4_ANIMAL_HPP
+
+#endif //LAB4_ANIMAL_HPP
